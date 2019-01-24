3ca90b4e038d2ab850b8fc561584bb4e2a132a81
JoanKingWC <JoanKingWC@163.com>
2019-01-24 14:37:39 +0800

    初始化

---
+++ JKDownLoader/JKDownLoader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JKDownLoader
+//
+//  Created by 王冲 on 2019/1/24.
+//  Copyright © 2019年 JK科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JKDownLoader/JKDownLoader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JKDownLoader
+//
+//  Created by 王冲 on 2019/1/24.
+//  Copyright © 2019年 JK科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
